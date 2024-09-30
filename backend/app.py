d24691c7db596ca372d312bca5cbaa8e243de659
Arsath mohamed <165041940+arsath-02@users.noreply.github.com>
2024-09-30 20:49:33 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -56,4 +56,5 @@ def _read_file_from_path(path):
     return data
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get('PORT', 8000))  # Get the port from environment variables or use 8000
+    app.run(host='0.0.0.0', port=port, debug=True)
